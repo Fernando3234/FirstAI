152a1030b188701ccc89ff3097786dd3f9a31ccb
danielgermany <onehander0@gmail.com>
2021-07-06 23:18:01 -0400

    Split some functionality.

--- FirstGridD.java
+++ FirstGridD.java
@@ -3,21 +3,23 @@ import java.awt.*;
 import javax.swing.*;
 
 class FirstGridD {
-    public static void main(String[] args) {
-      JFrame f;
-      f = new JFrame();
+  public static void CreateButtons(){
+    JFrame f;
+    f = new JFrame();
 
-      for(int x = 0; x <= 600; x+=300){
-        for(int y = 0; y <= 600; y+=300){
-          JButton button = new JButton("click");
-          button.setBounds(x,y,300,300);
-          f.add(button);
-        }
+    for(int x = 0; x <= 600; x+=300){
+      for(int y = 0; y <= 600; y+=300){
+        JButton button = new JButton("click");
+        button.setBounds(x,y,300,300);
+        f.add(button);
       }
-
-      f.setSize(900,900);
-      f.setLayout(null);
-      f.setVisible(true);
     }
-
+    f.setSize(900,900);
+    f.setLayout(null);
+    f.setVisible(true);
+  }
+  
+  public static void main(String[] args) {
+    CreateButtons();
+    }
 }
